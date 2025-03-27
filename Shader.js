eb4348d1c00a8acd91674d27ecfe534e25589388
himanshuraw <himanshurawat180@gmail.com>
2025-03-27 16:18:55 +0530

    Created shader and transformation class

---
+++ Shader.js
@@ -0,0 +1,53 @@
+export class Shader {
+    constructor(gl, vsSource, fsSource) {
+        this.gl = gl;
+        this.program = this.createProgram(vsSource, fsSource);
+    }
+
+    createProgram(vsSource, fsSource) {
+        const vertexShader = this.compileShader(this.gl.VERTEX_SHADER, vsSource);
+        const fragmentShader = this.compileShader(this.gl.FRAGMENT_SHADER, fsSource);
+
+        const program = this.gl.createProgram();
+        this.gl.attachShader(program, vertexShader);
+        this.gl.attachShader(program, fragmentShader);
+        this.gl.linkProgram(program);
+
+        if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
+            console.error('Error linking program', this.gl.getProgramInfoLog(program));
+            this.gl.deleteProgram(program);
+            return null;
+        }
+
+        return program;
+    }
+
+    compileShader(type, source) {
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            console.error('Error compiling shader', this.gl.getShaderInfoLog(shader));
+            this.gl.deleteShader(shader);
+            return null;
+        }
+        return shader;
+    }
+
+    getAttribute(name) {
+        return this.gl.getAttribLocation(this.program, name);
+    }
+
+    getUniform(name) {
+        return this.gl.getUniformLocation(this.program, name);
+    }
+
+    use() {
+        this.gl.useProgram(this.program);
+    }
+
+    setUniformMatrix(name, matrix) {
+        const location = this.gl.getUniformLocation(this.program, name);
+        this.gl.uniformMatrix4fv(location, false, matrix);
+    }
+}
---
+++ Transform.js
@@ -0,0 +1,42 @@
+import { vec3, mat4 } from 'https://cdn.skypack.dev/gl-matrix';
+export class Transform {
+    constructor() {
+        this.position = vec3.fromValues(0, 0, 0);
+        this.rotation = vec3.fromValues(0, 0, 0);
+        this.scale = vec3.fromValues(1, 1, 1);
+        this.modelMatrix = mat4.create();
+    }
+
+    setTranslate(x, y, z) {
+        vec3.set(this.position, x, y, z);
+    }
+
+    setRotate(x, y, z) {
+        vec3.set(this.rotation, x, y, z);
+    }
+
+    setScale(x, y, z) {
+        vec3.set(this.scale, x, y, z);
+    }
+
+    translateBy(dx, dy, dz) {
+        vec3.add(this.position, this.position, vec3.fromValues(dx, dy, dz));
+    }
+
+    rotateBy(dx, dy, dz) {
+        vec3.add(this.rotation, this.rotation, vec3.fromValues(dx, dy, dz));
+    }
+
+    scaleBy(sx, sy, sz) {
+        vec3.add(this.scale, this.scale, vec3.fromValues(sx, sy, sz));
+    }
+
+    updateModelMatrix() {
+        mat4.identity(this.modelMatrix);
+        mat4.translate(this.modelMatrix, this.modelMatrix, this.position);
+        mat4.rotateX(this.modelMatrix, this.modelMatrix, this.rotation[0]);
+        mat4.rotateY(this.modelMatrix, this.modelMatrix, this.rotation[1]);
+        mat4.rotateZ(this.modelMatrix, this.modelMatrix, this.rotation[2]);
+        mat4.scale(this.modelMatrix, this.modelMatrix, this.scale);
+    }
+}
